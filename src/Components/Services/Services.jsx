dd5eb100dadd13199b86b2a14ce5366fab1d7b08
Chinweike99 <innocentakwolu1@gmail.com>
2024-10-10 05:04:51 +0100

    updated

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -24,16 +24,18 @@ const Services = () => {
             <div className="serviceBox">
                 <CodeIcon  className='serviceIcon'/>
                 <h3>Performance Optimization</h3>
-                <p>
+                <p><small>
                 I create websites optimized for high search engine rankings (SEO) and performance. By implementing best practices in SEO and speed optimization, I ensure faster load times, better user experiences, and improved visibility in search engines.
+                </small>
                 </p>
                 
             </div>
             <div className="serviceBox">
                 <LayersIcon  className='serviceIcon'/>
                 <h3>Web Development</h3>
-                <p>
+                <p><small>
                 I'm passionate about web development and eager to learn and grow with others. I offer training and consultation in front-end development and the MERN stack. While new to teaching, I'm excited to share what I know and help others. For now, I'm offering free sessions, and look forward to growing together through collaboration.
+                </small>
                 </p>
             </div>
         </div>
