5e04bc258c377f4a5db7a59682d8b973a9818177
Chinweike99 <innocentakwolu1@gmail.com>
2024-10-24 02:47:47 +0100

    updated cv

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -23,7 +23,7 @@ const Home = () => {
             <div className="btnBox">
 
                 <span className='btn'>
-                    <a href="https://1drv.ms/b/c/988ee7b89f3aed84/EY7ANOfbJ_1Bomx7kkww8RQBrhA1jMSdZmd8bd3E1o30gQ" target='_blank' >
+                    <a href="https://1drv.ms/b/c/988ee7b89f3aed84/EZ3yPGyKL65CvkrjcCgHkvYBV39T23nAwEqBzoFDv1TdxQ" target='_blank' >
                         Download cv 
                     </a>
                 </span>
